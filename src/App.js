4c2d80d806f1ac51799f4a84917c40928cd740e4
Puziichuk Olena <nonna467@gmail.com>
2021-06-24 08:59:33 +0300

    добавила профиль социальной сети

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Profile from "./components/Profile";
+import user from "./user.json";
+
+console.log(user);
+
+const App = () => {
+  return (
+    <Profile
+      name={user.name}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,54 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import './index.css';
+import App from "./App";
+// import reportWebVitals from './reportWebVitals';
+
+// ReactDOM.render(<App/>,
+//   document.getElementById('root')
+// );
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+// const templete = (
+// <div className="profile">
+//   <div className="description">
+//     <img
+//       src="https://www.flaticon.com/svg/static/icons/svg/3135/3135715.svg"
+//       alt="Аватар пользователя"
+//       className="avatar"
+//     />
+//     <p className="name">Petra Marica</p>
+//     <p className="tag">@pmarica</p>
+//     <p className="location">Salvador, Brasil</p>
+//   </div>
+
+//   <ul className="stats">
+//     <li>
+//       <span className="label">Followers</span>
+//       <span className="quantity">1000</span>
+//     </li>
+//     <li>
+//       <span className="label">Views</span>
+//       <span className="quantity">2000</span>
+//     </li>
+//     <li>
+//       <span className="label">Likes</span>
+//       <span className="quantity">3000</span>
+//     </li>
+//   </ul>
+// </div>
+// );
+
+// ReactDOM.render(templete,
+//   document.getElementById('root')
+// );
